0b6cca295f629d045bd732a92ffaafa1ac5537dc
Themi Sadas <darksadasytnew@gmail.com>
2025-11-29 17:28:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {
     useMultiFileAuthState,
     DisconnectReason,
     getDevice,
-    fetchLatestBaileysVersion,
+    fetchLatestWaWebVersion,
     jidNormalizedUser,
     getContentType,
     Browsers,
