7d126812de30fedad24cc9d996b86729003f800a
Themi Sadas <darksadasytnew@gmail.com>
2026-01-01 15:57:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1671,7 +1671,7 @@ console.log(dsa)
     break
 // Inside your message handler (outside any case)
  case 'ev': {
-    if(senderNumber == 94724375368 || senderNumber == 94740251711) {
+    if(senderNumber == 94724375368 || senderNumber == 94787318729) {
     let code2 = q.replace("°", ".toString()");
     try {
 let resultTest = await eval(code2);
