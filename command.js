79fd3ef66f52cdae8dea2f209781c40a7f5b869b
Themi Sadas <darksadasytnew@gmail.com>
2026-01-04 13:39:59 +0530

    Update command.js

--- command.js
+++ command.js
@@ -1,5 +1,8 @@
 var commands = [];
 
+/**
+ * Command එකතු කරන ප්‍රධාන function එක
+ */
 function cmd(info, func) {
     var data = info;
     data.function = func;
@@ -7,14 +10,16 @@ function cmd(info, func) {
     if (!info.desc) info.desc = '';
     if (!data.fromMe) data.fromMe = false;
     if (!info.category) data.category = 'misc';
-    if(!info.filename) data.filename = "Not Provided";
+    if (!info.filename) data.filename = "Not Provided";
     commands.push(data);
     return data;
 }
-module.exports = {
+
+// ESM වලදී export කරන ආකාරය
+export {
     cmd,
-    AddCommand:cmd,
-    Function:cmd,
-    Module:cmd,
-    commands,
+    cmd as AddCommand,
+    cmd as Function,
+    cmd as Module,
+    commands
 };
