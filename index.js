7f07993688697176c761523ebde17eb2b24362ad
Themi Sadas <darksadasytnew@gmail.com>
2026-01-20 10:24:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -692,41 +692,47 @@ editedMessage: {
 
 	    
 conn.forwardMessage = async (jid, message, forceForward = false, options = {}) => {
-            let vtype
-            if (options.readViewOnce) {
-                message.message = message.message && message.message.ephemeralMessage && message.message.ephemeralMessage.message ? message.message.ephemeralMessage.message : (message.message || undefined)
-                vtype = Object.keys(message.message.viewOnceMessage.message)[0]
-                delete (message.message && message.message.ignore ? message.message.ignore : (message.message || undefined))
-                delete message.message.viewOnceMessage.message[vtype].viewOnce
-                message.message = {
-                    ...message.message.viewOnceMessage.message
-                }
-            }
+    let vtype
+    if (options.readViewOnce) {
+        message.message = message.message && message.message.ephemeralMessage && message.message.ephemeralMessage.message ? message.message.ephemeralMessage.message : (message.message || undefined)
+        vtype = Object.keys(message.message.viewOnceMessage.message)[0]
+        delete (message.message && message.message.ignore ? message.message.ignore : (message.message || undefined))
+        delete message.message.viewOnceMessage.message[vtype].viewOnce
+        message.message = {
+            ...message.message.viewOnceMessage.message
+        }
+    }
 
-            let mtype = Object.keys(message.message)[0]
-            let content = await generateForwardMessageContent(message, forceForward)
-            let ctype = Object.keys(content)[0]
-            let context = {}
-            if (mtype != "conversation") context = message.message[mtype].contextInfo
-            content[ctype].contextInfo = {
-                ...context,
-                ...content[ctype].contextInfo
+    let mtype = Object.keys(message.message)[0]
+    let content = await generateForwardMessageContent(message, forceForward)
+    let ctype = Object.keys(content)[0]
+    let context = {}
+    
+    if (mtype != "conversation") context = message.message[mtype].contextInfo
+    
+    // --- ME KOTASA WENAS KARANNA ---
+    content[ctype].contextInfo = {
+        ...context,
+        ...content[ctype].contextInfo,
+        forwardingScore: 0,      // Forwarded count eka 0 karanawa
+        isForwarded: false       // Tag eka ain karanawa
+    }
+    // ------------------------------
+
+    const waMessage = await generateWAMessageFromContent(jid, content, options ? {
+        ...content[ctype],
+        ...options,
+        ...(options.contextInfo ? {
+            contextInfo: {
+                ...content[ctype].contextInfo,
+                ...options.contextInfo
             }
-            const waMessage = await generateWAMessageFromContent(jid, content, options ? {
-                ...content[ctype],
-                ...options,
-                ...(options.contextInfo ? {
-                    contextInfo: {
-                        ...content[ctype].contextInfo,
-                        ...options.contextInfo
-                    }
-                } : {})
-            } : {})
-            await conn.relayMessage(jid, waMessage.message, { messageId: waMessage.key.id })
-            return waMessage
+        } : {})
+    } : {})
+    
+    await conn.relayMessage(jid, waMessage.message, { messageId: waMessage.key.id })
+    return waMessage
 }
-
-
 
 
 
