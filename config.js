02c4afce1aed7ccf41bb0c13cb2bfc91ff770932
Themi-svg <121729873+Themi-svg@users.noreply.github.com>
2026-03-02 14:57:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 //gg
 module.exports = {
-SESSION_ID:'2BBgFIgL#8xnKxs2easZ98nXkA31lW84Q_bMoln3gkw_IdoCjeiI',
+SESSION_ID:'zAVWmbBK#9PxnwiWO7smV94CQAxH2Gdj24600q1qUyB8WbQ51Bw0',
 
 ANTI_DELETE: process.env.ANTI_DELETE === undefined ? 'true' : process.env.ANTI_DELETE, 
 MV_BLOCK: process.env. MV_BLOCK === undefined ? 'true' : process.env. MV_BLOCK,    
