7c3cb348ddaf5c1689c1bb164affe9646a53e27b
Themi Sadas <darksadasytnew@gmail.com>
2026-03-08 21:24:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 //gg
 module.exports = {
-SESSION_ID:'2hZ0iD5Y#nE2jeq3mNvHDviyaXs63gQqYtVi1cfcasRzC08TnMd0',
+SESSION_ID:'2wIkQLwb#yutQj1bLqwdr8Uo98R5siqGUGm_u3FgbHBkaA3wpRbw',
 
 ANTI_DELETE: process.env.ANTI_DELETE === undefined ? 'true' : process.env.ANTI_DELETE, 
 MV_BLOCK: process.env. MV_BLOCK === undefined ? 'true' : process.env. MV_BLOCK,    
